e68ba3e0a2cb0c2177a5cf3ab384854ddd6bf741
Ponglada <ponglada@Davids-MacBook-Pro-4.local>
2024-03-06 21:43:06 +1100

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+// 1. Store the delete button in a deleteBtn variable
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("save-btn")
+let leadsFromlocalstorage = JSON.parse(localStorage.getItem("myLeads"))
+
+
+tabBtn.addEventListener("click", function() {
+    // Grab the url of the current tab
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+      })
+} )
+
+
+if (leadsFromlocalstorage) {
+    myLeads = leadsFromlocalstorage
+    render(myLeads)
+}
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems  
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    // save myLeads array to localstorage
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
+
+console.log(localStorage.getItem("myLeads"))
+
+
